0f6ef0e503eff90ee17be2892017b2faad167233
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-06-11 11:14:53 +0500

    Add Button EventClickListener Added

--- Lec-11_12ListViews/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Lec-11_12ListViews/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    Button btnAdd;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.tasks);
+        ArrayList<String> tasks = new ArrayList<>();
+        tasks.add("Task1 added");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tasks);
+        listView.setAdapter(arrayAdapter);
+
+        textview = findViewById(R.id.editText);
+        btnAdd = findViewById(R.id.button);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tasks.add(textview.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+                textview.setText("");
+            }
+        });
+
     }
 }
